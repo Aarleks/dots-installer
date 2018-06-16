2a97e23a31e648d6196171506845c91014ad456d
Alex Norman <a.norman@westernsydney.edu.au>
2018-06-16 14:59:24 +1000

    deleted old configs and edited testshell.sh for Arch

--- testshell.sh
+++ testshell.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 
-i3-msg "workspace 5; append_layout /home/alex/.config/i3/thing.json"
+i3-msg "workspace 7; append_layout ~/.config/dotfiles/thing.json"
 
 sleep 1
-i3-msg "workspace 5; exec st -e bash -c 'vim .config/i3/config && bash'"
+i3-msg "workspace 7; exec urxvt -e bash -c 'cmatrix && bash'"
 
 sleep 0.5
-i3-msg "workspace 5; exec st -e bash -c 'neofetch && bash'"
+i3-msg "workspace 7; exec urxvt -e bash -c 'neofetch && bash'"
 
 sleep 0.5
-i3-msg "workspace 5; exec st -e bash -c 'cmatrix && bash'"
+i3-msg "workspace 7; exec urxvt -e bash -c 'ranger && bash'"
