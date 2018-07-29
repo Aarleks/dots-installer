09ee25521d4a5e64ab345bb9074552290255aa4b
Alex Norman <a.norman@westernsydney.edu.au>
2018-07-29 23:07:57 +1000

    Added a brew installer file.

---
+++ brew.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+brew update
+
+brew upgrade
+
+brew install bash
+brew install pandoc
+brew install evernote
+brew install vim
+
--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 # This be the script to install all me things, just like a magic trick
 
 # Things I need to do.
-# copy .vimrc to both ~/.vimrc and ~/.gvimrc
+# copy to ~/
+#	.vimrc to both ~/.vimrc and ~/.gvimrc
+#	.vim folder
+#	.bashrc file
