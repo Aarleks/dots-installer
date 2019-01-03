62dd97cf3e5260feb2de7eec0f298e72bac7f964
Alex Norman <a.norman@westernsydney.edu.au>
2019-01-03 14:18:53 +1100

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-# This be the script to install all me things, just like a magic trick
-
-# Things I need to do.
-# copy to ~/
-#	.vimrc to both ~/.vimrc and ~/.gvimrc
-#	.vim folder
-#	.bashrc file
